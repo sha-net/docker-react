34eab91d740e4f083fd28b2f9219689388db7fb4
Shavit Netzer <shavitne@payoneer.com>
2018-10-30 19:48:19 +0200

    change travis-ci script

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const redis = require('redis');
-const process = require('process');
+//const process = require('process');
 const app = express();
 const client = redis.createClient({
  host: 'redis-server',
@@ -8,7 +8,7 @@ const client = redis.createClient({
 });
 client.set('visits',0);
 app.get('/',(req,res) => {
- process.exit(1);
+// process.exit(1);
  client.get('visits',(err,visits) => {
  res.send('Number of visits is ' + visits);
  client.set('visits',parseInt(visits) +1);
